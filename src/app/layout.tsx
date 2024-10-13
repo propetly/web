332caf6f49d2726fac5d3706a625e5423254916f
NickRisinger <boozy044@gmail.com>
2024-10-13 12:50:08 +0300

    feat: change app fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+const interSans = Inter();
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body style={interSans.style} className="antialiased">
         {children}
       </body>
     </html>
